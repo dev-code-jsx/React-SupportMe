ccc8d76e5b94d0f817e89f8dfe6c2781d5d423f7
jceferino-2022238 <jceferino-2022238@kinal.edu.gt>
2024-07-22 13:21:35 -0600

    Fix for the resource card content

--- src/components/ResourceCardPatient.jsx
+++ src/components/ResourceCardPatient.jsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
 export const ResourceCardPatient = ({ titulo, imagenRecurso, tipo, contenido, _id }) => {
     //hacer que cuando toque el boton de go me suba el id a la url y luego ese id lo busque en la base de datos y muestre el recurso
-
+    const shortenText = (text, maxLength) => {
+        if (text.length <= maxLength) return text;
+        return text.substring(0, maxLength) + '...';
+    };
     return (
         <div className="rounded-lg overflow-hidden shadow-lg">
             <img
@@ -13,16 +16,16 @@ export const ResourceCardPatient = ({ titulo, imagenRecurso, tipo, contenido, _i
             />
             <div className="p-4 space-y-2">
                 <h3 className="text-xl font-semibold">{titulo}</h3>
-                <div className="flex items-center justify-between">
-                    <span className="text-muted-foreground">{tipo}</span>
-                    <p className="text-muted-foreground">{contenido}</p>
-                    <Link
-                        to={`/principal/resources/${_id}`}
-                        className="inline-flex h-8 items-center justify-center rounded-md bg-primary px-4 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
-                    >
-                        Go
-                    </Link>
-                </div>
+                <span className="text-muted-foreground text-base">{tipo}</span>
+                <p className="text-muted-foreground text-justify">
+                    {shortenText(contenido, 150)} {/* Adjust the maxLength as needed */}
+                </p>
+                <Link
+                    to={`/principal/resources/${_id}`}
+                    className="inline-flex h-8 items-center justify-center rounded-md bg-primary px-4 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 mt-2"
+                >
+                    Go
+                </Link>
             </div>
         </div>
     );
