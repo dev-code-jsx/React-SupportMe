c84ab1f41fc12e150a7205c2dd13449d558baad4
echamale <echamale-2022222@kinal.edu.gt>
2024-07-22 14:14:55 -0600

    modificando estilos de recursos pacientes

--- src/components/BlogResourcePatient.jsx
+++ src/components/BlogResourcePatient.jsx
@@ -8,7 +8,7 @@ export const BlogArticlePatient = ({ recurso }) => {
                     <div className="inline-block rounded-full bg-gray-200 px-3 py-1 text-sm text-gray-700 dark:bg-gray-700 dark:text-gray-300">
                         {recurso.tipo}
                     </div>
-                    <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl text-gray-900 dark:text-white">
+                    <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl text-black ">
                         {recurso.titulo}
                     </h1>
                     <p className="text-gray-600 dark:text-gray-400">
@@ -23,7 +23,7 @@ export const BlogArticlePatient = ({ recurso }) => {
                     />
                     <figcaption className="text-center text-gray-500 mt-2">Imagen de portada</figcaption>
                 </figure>
-                <div className="mt-8 text-lg leading-relaxed text-gray-700 dark:text-gray-300">
+                <div className="mt-8 text-lg leading-relaxed text-gray-700 ">
                     {recurso.contenido}
                 </div>
                 <div className="flex justify-start mt-10">
--- src/pages/BlogPage.jsx/BlogPage.jsx
+++ src/pages/BlogPage.jsx/BlogPage.jsx
@@ -11,8 +11,6 @@ export const BlogPage = () => {
     if (error) {
         return <div>Error loading resource</div>;
     }
-    //aqui hay dos fformas de hacerlo mandan el id y aqui solo lo envian o obtienen el id de la url.
-    //agregar el hook que busque el articulo en la base de datos por id y obtener esa data en la variable article
 
     return (
         <div>
